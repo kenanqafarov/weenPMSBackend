9b3a055fc49932bdd696f5a964491058598ee166
kenanqafarov <qafarovkenan06@gmail.com>
2026-04-25 21:00:51 +0400

    Server ready

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { notFound, errorHandler } = require('./middleware/errorHandler');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 
